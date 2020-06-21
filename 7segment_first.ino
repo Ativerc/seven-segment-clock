1168b5e8a605ac9f869fe979fba22d06f71979e0
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2020-06-21 21:21:55 +0530

    Counting down from 9-0 on a single CC7seg display module.

--- 7segment_first.ino
+++ 7segment_first.ino
@@ -1,7 +1,12 @@
 /*
 Code initially from allaboutcircuits
-I just added some comments. 
-Code is very easy to understand.
+What this does:
+This is making a 7 segment display count from 9 to 0 
+with a 1 second delay in between each change,
+
+Code is more complex than the first commit's code for 
+just interfacing with the 7 segment display module and 
+turning on/off all its segments one by one.
 
 Pinout: Common Cathode 7 segment display 
 
@@ -23,32 +28,55 @@ Wiring: 7 segment display module with Arduino Uno
 |10g|D8|
 */ 
 
-void setup()
+// make an array to save Sev Seg pin configuration of numbers
+
+int num_array[10][7] = {  { 1,1,1,1,1,1,0 },    // 0
+                          { 0,1,1,0,0,0,0 },    // 1
+                          { 1,1,0,1,1,0,1 },    // 2
+                          { 1,1,1,1,0,0,1 },    // 3
+                          { 0,1,1,0,0,1,1 },    // 4
+                          { 1,0,1,1,0,1,1 },    // 5
+                          { 1,0,1,1,1,1,1 },    // 6
+                          { 1,1,1,0,0,0,0 },    // 7
+                          { 1,1,1,1,1,1,1 },    // 8
+                          { 1,1,1,1,0,1,1 }};   // 9
+//function header
+void Num_Write(int);
+
+void setup() 
 { 
-  // defining pins and their respective modes
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
-  pinMode(8,OUTPUT);
+  // set pin modes
+  pinMode(2, OUTPUT);   
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop() 
 {
-  // loop to turn the LED segments ON. ONE by ONE.
-  for(int i=2;i<9;i++)
+  // This function sends the actual number that has to be displayed to the End User,
+  // to the Num_write function.
+  for (int counter = 10; counter > 0; counter--) 
   {
-    digitalWrite(i,HIGH);
-    delay(600);
+    delay(1000);
+    Num_Write(counter-1); 
   }
+  delay(3000);
+}
 
-  // loop to turn the LED segments OFF. ONE by ONE.
-  for(int i=2;i<9;i++)
+// This function receives the number to be displayed 
+// It fetches the value for each segment (from the num array),
+// for the number to be displayed.
+// And sends the value to the corresponding pin of the 7 seg display.
+void Num_Write(int number) 
+{
+  int pin= 2;
+  for (int j=0; j < 7; j++) 
   {
-    digitalWrite(i,LOW);
-    delay(600);
+    digitalWrite(pin, num_array[number][j]);
+    pin++;
   }
-  delay(1000);
 }
