8ff799694c8568bcf360bce697530f68dad04505
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2020-06-22 12:27:47 +0530

    Empty code

---
+++ 7segment.ino
@@ -0,0 +1,28 @@
+/*
+Pinout: Common Cathode 7 segment display 
+
+10g 9f 8GND 7a 6b
+
+1e  2d 3GND 4c 5dp
+
+Wiring: 7 segment display module with Arduino Uno
+| 7 segment | Arduino Uno |
+|---|---|
+|1e|D6|
+|2d|D5|
+|3,8|GND|
+|4c|D4|
+|5dp|-|
+|6b|D3|
+|7a|D2|
+|9f|D7|
+|10g|D8|
+*/ 
+
+void setup() {
+    
+}
+
+void loop() {
+
+}
